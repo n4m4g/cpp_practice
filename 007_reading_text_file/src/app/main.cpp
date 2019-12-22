9b68502a981f614f555794f32dfc7637ec28f511
Chun-Hsien Huang <n4m4g64b4@gmail.com>
2019-12-22 20:22:11 +0800

    modify project layout, add comment to main.cpp

--- 007_reading_text_file/src/app/main.cpp
+++ 007_reading_text_file/src/app/main.cpp
@@ -8,17 +8,20 @@ int main() {
 	fstream inFile;
 
 	// file name
-	string fname = "../text.txt";
+	string fname = "../../src/app/text.txt";
 
 	// inFile.open(fname);
 	inFile.open(fname, ios::in);
 
+	// check whether the file is open
 	if(inFile.is_open()) {
+		// a temp string variable to store line from getline
 		string line;
 		while(inFile) {
 			getline(inFile, line);
 			cout << line << endl;
 		}
+		// close the file after reading
 		inFile.close();
 	} else {
 		cout << "Could not open file: " << fname << endl;
